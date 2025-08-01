c774535cf775274c4eb6debdf3f49cc58a992762
Nic Simms <nsimms@asu.edu>
2025-07-31 22:07:08 -0700

    add Regular Expressions

--- Chapter-2/crash_course.py
+++ Chapter-2/crash_course.py
@@ -52,3 +52,19 @@ one_friend = random.choice(["alice", "bob", "john"])
 #Output: bob
 #Explanation: make a random choice of given list. 
 
+"""
+Regular Expressions
+"""
+import re
+
+re_examples = [
+    not re.match("a", "cat"),
+    re.search("a", "cat"),
+    not re.search("c", "dog"),
+    3 == len(re.split("[ab]", "carbs")),
+    "R-D-" == re.sub("[0-9]", "-", "R2D2")
+]
+#print(re_examples)
+#Output: [True, <re.Match object; span=(1, 2), match='a'>, True, True, True]
+#Explanation: re.match is True if the start of a string matches, 
+# re.search checks any part for a match.
