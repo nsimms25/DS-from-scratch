8b453b10340800ca96bdbd084bddf7eeefde9e38
Nic Simms <nsimms@asu.edu>
2025-08-07 18:15:45 -0700

    add misleading plot.

--- Chapter-3/visualization.py
+++ Chapter-3/visualization.py
@@ -42,3 +42,17 @@ plt.xlabel("Decile")
 plt.ylabel("# of Students")
 plt.title("Distribution of Exam 1 Grades")
 plt.show()
+
+#Be careful, using plt can lead to potentially mmisleading visuals
+mentions = [500, 505]
+years = [2017, 2018]
+
+plt.bar(years, mentions, 0.8)
+plt.xticks(years)
+plt.ylabel("# of times I heard Data Science")
+
+plt.ticklabel_format(useOffset=False)
+
+plt.axis([2016.5, 2018.5, 499, 506])
+plt.title("Look at the Increase")
+plt.show()
