32fb641ffcbc10b794eae4c25f8c8b9f006f0ec6
Nic Simms <nsimms@asu.edu>
2025-08-07 17:50:51 -0700

    students histogram

--- Chapter-3/visualization.py
+++ Chapter-3/visualization.py
@@ -24,3 +24,21 @@ plt.ylabel("# of Academy Awards")
 plt.xticks(range(len(movies)), movies)
 
 plt.show()
+
+from collections import Counter
+grades = [83, 95, 91, 87,70, 0, 85, 82, 100, 67, 73, 77, 0]
+
+histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+
+plt.bar([x + 5 for x in histogram.keys()],
+        histogram.values(),
+        10,
+        edgecolor = (0, 0, 0))
+
+plt.axis([-5, 105, 0, 5])
+
+plt.xticks([10 * i for i in range(11)])
+plt.xlabel("Decile")
+plt.ylabel("# of Students")
+plt.title("Distribution of Exam 1 Grades")
+plt.show()
